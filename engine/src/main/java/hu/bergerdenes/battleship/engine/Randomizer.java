b1f2410c6f80a6c01b1dd5844dae51aa24aaf263
Dénes Berger <berger.denes@gmail.com>
2026-08-27 00:37:39 +0200

    Refactor `Randomizer` and enhance tests with boundary checks and nested test classes

--- engine/src/main/java/hu/bergerdenes/battleship/engine/Randomizer.java
+++ engine/src/main/java/hu/bergerdenes/battleship/engine/Randomizer.java
@@ -2,24 +2,25 @@ package hu.bergerdenes.battleship.engine;
 
 import java.security.SecureRandom;
 
-class Randomizer {
+import hu.bergerdenes.battleship.interfaces.Point;
+
+public class Randomizer {
 
     private static final SecureRandom secureRandom = new SecureRandom();
 
     private final int boardSize;
 
-    Randomizer(int boardSize) {
+    public Randomizer(int boardSize) {
         this.boardSize = boardSize;
     }
 
-    // TOOD: update test! check for bounds!
-    Point getRandomPoint(int shipSize, Orientation orientation) {
-        int row = secureRandom.nextInt(boardSize - (orientation == Orientation.HORIZONTAL ? shipSize : 1));
-        int col = secureRandom.nextInt(boardSize - (orientation == Orientation.VERTICAL ? shipSize : 1));
+    public Point getRandomPoint(int shipSize, Orientation orientation) {
+        int row = secureRandom.nextInt(boardSize - (orientation == Orientation.VERTICAL ? shipSize - 1: 0));
+        int col = secureRandom.nextInt(boardSize - (orientation == Orientation.HORIZONTAL ? shipSize - 1 : 0));
         return new Point(row, col);
     }
 
-    Orientation getRandomOrientation() {
+    public Orientation getRandomOrientation() {
         return secureRandom.nextBoolean() ? Orientation.HORIZONTAL : Orientation.VERTICAL;
     }
 }
--- engine/src/test/java/hu/bergerdenes/battleship/engine/RandomizerTest.java
+++ engine/src/test/java/hu/bergerdenes/battleship/engine/RandomizerTest.java
@@ -1,33 +1,83 @@
 package hu.bergerdenes.battleship.engine;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 
+import hu.bergerdenes.battleship.interfaces.Point;
+
 class RandomizerTest {
+
+    private static final int BOARD_SIZE = 10;
 
     private Randomizer underTest;
 
     @BeforeEach
     void setUp() {
-        underTest = new Randomizer(10);
+        underTest = new Randomizer(BOARD_SIZE);
     }
 
-    @RepeatedTest(value = 100)
-    void testGetRandomPointVertical() {
-        Point point = underTest.getRandomPoint(5, Orientation.HORIZONTAL);
-        System.out.printf("Generated point: (%d, %d)%n", point.row(), point.column());
-        assertTrue(point.row() >= 0 && point.row() <= 5);
-        assertTrue(point.column() >= 0 && point.column() <= 9);
+    @Nested
+    class Horizontal {
+
+        private static final int SHIP_SIZE = 5;
+
+        private static int min = Integer.MAX_VALUE;
+        private static int max = Integer.MIN_VALUE;
+
+        @RepeatedTest(value = 100)
+        void testGetRandomPointHorizontal() {
+            Point point = underTest.getRandomPoint(SHIP_SIZE, Orientation.HORIZONTAL);
+            System.out.printf("Generated point: (%d, %d)%n", point.row(), point.column());
+            if (point.column() < min) {
+                min = point.column();
+            }
+            if (point.column() > max) {
+                max = point.column();
+            }
+            assertThat(point.row()).isBetween(0, BOARD_SIZE - 1);
+            assertThat(point.column()).isBetween(0, BOARD_SIZE - SHIP_SIZE);
+        }
+
+        @AfterAll()
+        static void afterAll() {
+            assertThat(min).withFailMessage("Min value should be 0 but it was " + min).isEqualTo(0);
+            assertThat(max).withFailMessage(String.format("Max value should be %d but it was %d", BOARD_SIZE - SHIP_SIZE, max)).isEqualTo(BOARD_SIZE - SHIP_SIZE);
+            min = Integer.MAX_VALUE;
+            max = Integer.MIN_VALUE;
+        }
+
     }
 
-    @RepeatedTest(value = 100)
-    void testGetRandomPointHorizontal() {
-        Point point = underTest.getRandomPoint(5, Orientation.VERTICAL);
-        System.out.printf("Generated point: (%d, %d)%n", point.row(), point.column());
-        assertTrue(point.row() >= 0 && point.row() <= 9);
-        assertTrue(point.column() >= 0 && point.column() <= 5);
-    }
+    @Nested
+    class Vertical {
 
+        private static final int SHIP_SIZE = 4;
+        private static int min = Integer.MAX_VALUE;
+        private static int max = Integer.MIN_VALUE;
+
+        @RepeatedTest(value = 100)
+        void testGetRandomPointVertical() {
+            Point point = underTest.getRandomPoint(SHIP_SIZE, Orientation.VERTICAL);
+            System.out.printf("Generated point: (%d, %d)%n", point.row(), point.column());
+            if (point.row() < min) {
+                min = point.row();
+            }
+            if (point.row() > max) {
+                max = point.row();
+            }
+            assertThat(point.row()).isBetween(0, BOARD_SIZE - SHIP_SIZE);
+            assertThat(point.column()).isBetween(0, BOARD_SIZE - 1);
+        }
+
+        @AfterAll
+        static void afterAll() {
+            assertThat(min).withFailMessage("Min value should be 0 but it was " + min).isEqualTo(0);
+            assertThat(max).withFailMessage(String.format("Max value should be %d but it was %d", BOARD_SIZE - SHIP_SIZE, max)).isEqualTo(BOARD_SIZE - SHIP_SIZE);
+        }
+
+    }
 }
